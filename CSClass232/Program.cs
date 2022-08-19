e62a23896f9d1e4b641e644712c637dd766fd7ea
Gihun Ham <ham@e-mirim.hs.kr>
2022-08-19 11:07:27 +0900

    #3 out 키워드 실습

--- CSClass232/Program.cs
+++ CSClass232/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +24,31 @@ namespace CSClass232
             Console.WriteLine("오늘의 점심 메뉴는 " + p[2] + "입니다.");
             p[2] = "단무지";
             Console.WriteLine("오늘의 점심 메뉴는 " + p[2] + "입니다.");
+
+            Console.WriteLine("숫자 입력: ");
+            //int output;
+            bool result = int.TryParse(Console.ReadLine(), out int output);
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자" + output);
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력해주세요!");
+            }
+            int x = 0;
+            int y = 0;
+            int vx = 1;
+            int vy = 1;
+            Console.WriteLine("현재 좌표: (" + x + "," + y + ")");
+            NextPos(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음 좌표: (" + x + "," + y + ")");
+        }
+
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
         }
     }
 }
